8417f8c22a157c188669213545cb6debff312294
tylerjusfly <tylerjusfly1@gmail.com>
2022-06-22 16:14:18 +0100

    setting up

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express, { Application, ErrorRequestHandler } from "express";
+import { Server } from "http";
+
+// Setting app to type of express application
+const app: Application = express();
+
+const PORT = process.env.PORT || 4000;
+
+// setting up Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    status: err.status || 500,
+    message: err.message,
+  });
+};
+
+app.use(errorHandler);
+
+const server: Server = app.listen(PORT, () => {
+  console.log(`The 👨‍🏭 server 🚗 is 🏃‍♀️ running 👡 on ⚓ port 🐹 ${PORT}, 🛒 let's 💅 go 😝 catch 🙀 it! 🍟`);
+});
